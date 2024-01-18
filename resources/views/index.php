e50d804143bd89a639f159bb7ee828ee5a5816f9
Ana Maria Correa <111539281+AnaC1997@users.noreply.github.com>
2024-01-18 14:22:23 +0100

    first commit

---
+++ resources/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP BADWORDS</title>
+    <?php $messaggio = "Hello word." ?>
+</head>
+<body>
+    <h1> <?php echo $messaggio ?> </h1>
+</body>
+</html>
+
